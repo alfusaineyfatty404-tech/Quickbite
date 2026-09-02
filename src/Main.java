f7f62b0c706dc09d3369eb813fb38b1bb5ee3202
alfusaineyfatty.tech <alfusaineyfatty404@gmail.com>
2026-09-02 02:17:55 -1200

    feat: model menu items with parallel arrays

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Qickbite
+        /*
+        itemName: data type for this Array is string
+        itemPrices: data type for this Array is double
+        itemAvailable: data type for this Array is boolean
+         */
+
+        String[] itemName = {"banana", "mango", "orange"};
+        double[] itemPrices = {9.5, 2.5, 5.2};
+        boolean[] itemAvailable = {true, true, true};
+
+        // Print the items one-by-one
+        for (int i = 0; i < itemName.length; i++) {
+            // check the availability of an item and print
+            // sold out or available
+            String check  = null;
+            if (itemAvailable[i] == true){
+                    check ="Available"
+                    }else{
+                            check = "Sold out";
+        }
+            System.out.println(itemName[i])+ " "+itemPrices[i]+ " "+check);
+        }
+    }
+}
